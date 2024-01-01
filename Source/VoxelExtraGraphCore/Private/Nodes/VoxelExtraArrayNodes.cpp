c301e46969395d632f8480dfd54f2fbcb152a8ae
DanWaheed <117957636+DanWaheed@users.noreply.github.com>
2024-01-01 00:17:17 -0500

    Add Array Index Set

--- Source/VoxelExtraGraphCore/Private/Nodes/VoxelExtraArrayNodes.cpp
+++ Source/VoxelExtraGraphCore/Private/Nodes/VoxelExtraArrayNodes.cpp
@@ -1,5 +1,6 @@
 #include "Nodes/VoxelExtraArrayNodes.h"
 #include "VoxelBufferUtilities.h"
+#include "VoxelBufferBuilder.h"
 
 DEFINE_VOXEL_NODE_COMPUTE(FVoxelNode_ArrayGetItemBuffered, Result)
 {
@@ -18,8 +19,7 @@ DEFINE_VOXEL_NODE_COMPUTE(FVoxelNode_ArrayGetItemBuffered, Result)
 		{
 			int32 Value = Index[ValueIndex];
 
-			if (Value < 0 ||
-				Value >= NumValues)
+			if (Value < 0 || Value >= NumValues)
 			{
 				Indices[ValueIndex] = 0;
 				continue;
@@ -50,5 +50,61 @@ void FVoxelNode_ArrayGetItemBuffered::PromotePin(FVoxelPin& Pin, const FVoxelPin
 {
 	GetPin(ValuesPin).SetType(NewType.GetBufferType().WithBufferArray(true));
 	GetPin(ResultPin).SetType(NewType.GetInnerType().GetBufferType());
+}
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+DEFINE_VOXEL_NODE_COMPUTE(FVoxelNode_ArraySetItem, Result)
+{
+	const TValue<FVoxelBuffer> Values = GetNodeRuntime().Get<FVoxelBuffer>(ValuesPin, Query);
+	const TValue<int32> Index = GetNodeRuntime().Get<int32>(IndexPin, Query);
+	const FVoxelFutureValue Value = GetNodeRuntime().Get(ValuePin, Query);
+
+	return VOXEL_ON_COMPLETE(Values, Index, Value)
+	{
+		const int32 NumValues = Values->Num();
+		if (Index < 0 || Index >= NumValues)
+		{
+			VOXEL_MESSAGE(Error, "{0}: Invalid Index {1}. Values.Num={2}", this, Index, NumValues);
+			return {};
+		}
+
+		const TSharedRef<FVoxelBufferBuilder> BufferBuilder = MakeVoxelShared<FVoxelBufferBuilder>(Values->GetInnerType());
+		for (int32 ValueIndex = 0; ValueIndex < NumValues; ValueIndex++)
+		{
+			if (ValueIndex == Index)
+			{
+				BufferBuilder->Add(Value);
+				continue;
+			}
+
+			BufferBuilder->Add(Values->GetGeneric(ValueIndex));
+		}
+
+		return FVoxelRuntimePinValue::Make(BufferBuilder->MakeBuffer(), ReturnPinType);
+	};
+}
+
+#if WITH_EDITOR
+FVoxelPinTypeSet FVoxelNode_ArraySetItem::GetPromotionTypes(const FVoxelPin& Pin) const
+{
+	if (Pin.Name == ValuesPin || Pin.Name == ResultPin)
+	{
+		return FVoxelPinTypeSet::AllBufferArrays();
+	}
+	else
+	{
+		return FVoxelPinTypeSet::AllUniforms();
+	}
+}
+
+void FVoxelNode_ArraySetItem::PromotePin(FVoxelPin& Pin, const FVoxelPinType& NewType)
+{
+	GetPin(ValuesPin).SetType(NewType.GetBufferType().WithBufferArray(true));
+	GetPin(ValuePin).SetType(NewType.GetInnerType());
+	GetPin(ResultPin).SetType(NewType.GetBufferType().WithBufferArray(true));
 }
 #endif
--- Source/VoxelExtraGraphCore/Public/Nodes/VoxelExtraArrayNodes.h
+++ Source/VoxelExtraGraphCore/Public/Nodes/VoxelExtraArrayNodes.h
@@ -24,3 +24,28 @@ public:
 #endif
 	//~ End FVoxelNode Interface
 };
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// Given an array and an index and a value, sets the item in the array at that index
+USTRUCT(Category = "Array", meta = (DisplayName = "Set", CompactNodeTitle = "SET", Keywords = "item array"))
+struct VOXELEXTRAGRAPHCORE_API FVoxelNode_ArraySetItem : public FVoxelNode
+{
+	GENERATED_BODY()
+	GENERATED_VOXEL_NODE_BODY()
+
+public:
+	VOXEL_INPUT_PIN(FVoxelWildcardBuffer, Values, nullptr, ArrayPin);
+	VOXEL_INPUT_PIN(int32, Index, 0);
+	VOXEL_INPUT_PIN(FVoxelWildcard, Value, nullptr);
+	VOXEL_OUTPUT_PIN(FVoxelWildcardBuffer, Result);
+
+	//~ Begin FVoxelNode Interface
+#if WITH_EDITOR
+	virtual FVoxelPinTypeSet GetPromotionTypes(const FVoxelPin& Pin) const override;
+	virtual void PromotePin(FVoxelPin& Pin, const FVoxelPinType& NewType) override;
+#endif
+	//~ End FVoxelNode Interface
+};
